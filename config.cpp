12aa36169784317cb9540083165f0950cb6dec09
Gefaehrbert <benedict@degner-elsner.net>
2023-06-13 19:19:28 +0200

    Update config.cpp

--- config.cpp
+++ config.cpp
@@ -1,21 +1,3 @@
-class CfgVehicles
-{
-	class Inventory_Base;
-	class Edible_Base;
-	class MilkaChocolate: Zagorky_ColorBase
-	{
-		scope=2;
-		displayName="Milka Chocolate";
-		descriptionShort="Fines German Chocolate";
-		hiddenSelections[]=
-		{
-			"camoground"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"MilkaCow\data\MilkaChocolate_co.paa"
-		};
-	
 class CfgPatches
 
 {
@@ -29,14 +11,35 @@ class CfgPatches
 		requiredVersion=1.18;
 		requiredAddons[]=
 		{
-			"DZ_Animals"
+			"DZ_Animals",
+			"DZ_Data",
+			"DZ_Gear_Food",
+			"DZ_Gear_Drinks"
 		};
 	};
 };
+
+
+	class MilkaChocolate: Zagorky_ColorBase
+	{
+		scope=2;
+		displayName="Milka Chocolate";
+		descriptionShort="Fines German Chocolate";
+		hiddenSelections[]=
+		{
+			"camoground"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"MilkaCow\data\MilkaChocolate_co.paa"
+		};
+
 class CfgVehicles
 {
 	class Animal_BosTaurusF;
 	class Animal_BosTaurusF_Spotted;
+	class Inventory_Base;
+	class Edible_Base;
 	class MilkaCow: Animal_BosTaurusF_Spotted
 	{
 		scope=2;
@@ -57,4 +60,18 @@ class CfgVehicles
 			};
 		};
 	};
+	class MilkaChocolate: Zagorky_ColorBase
+	{
+		scope=2;
+		displayName="Milka Chocolate";
+		descriptionShort="Fines German Chocolate";
+		hiddenSelections[]=
+		{
+			"camoground"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"MilkaCow\data\MilkaChocolate_co.paa"
+		};
+
 };
